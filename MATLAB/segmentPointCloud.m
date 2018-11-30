876aeb9142e4641625de52c8ee5249700e1c67ac
swhiteinventor <32525855+swhiteinventor@users.noreply.github.com>
2018-11-30 14:29:57 -0500

    fixed title errors in segmenting

--- MATLAB/segmentPointCloud.m
+++ MATLAB/segmentPointCloud.m
@@ -2,24 +2,24 @@
 % This function takesi n a point cloud and groups the points into
 % clusters that are seperated by the given minimum distance. This function
 % can also plot the clusters.
-function [labels,numClusters] = segmentPointCloud(ptCloud, minDistance, print, plot, timer)
+function [labels, numClusters] = segmentPointCloud(ptCloud, minDistance, print, plot, timer)
 %%
 % Segment the point cloud data:
 [labels,numClusters] = pcsegdist(ptCloud,minDistance);
 %%
 % Prints the number of clusters:
 if print
-    fprint('Number of Clusters: %d', numClusters);
+    fprintf('Number of Clusters: %d.\n', numClusters);
 end
 %% 
 % Plots the clusters
 if plot
-    title('Point Cloud Clusters')
-    figure('name',title);
+    name = 'Point Cloud Clusters';
+    figure('Name',name);
     hold on;
     pcshow(ptCloud.Location,labels);
     colormap(hsv(numClusters));
-    title(title);
+    title(name);
 end
 
 end
